501ef0e4ffd53228d2d24122b101219dff668438
Perumal, Arun Kumar (Cognizant) <574097@cognizant.com>
2018-03-29 10:04:49 +0000

    initial commit

---
+++ devopsdemoproject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace devopsdemoproject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
